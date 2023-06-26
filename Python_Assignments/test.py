bedcf556ad609d644ed16f149a9627cd8c8edca4
Vamsi Maradana <vamsitesting@yahoo.com>
2023-06-26 13:39:54 +0100

    added python Files

---
+++ Python_Assignments/test.py
@@ -0,0 +1,16 @@
+#print ('Hello World')
+#x = 10
+#print (type(x))
+#print(f"X value is {x}")
+#y=2.4
+#print ("Y type is :" + str(type(y)))
+#z=True
+#print (type(z))
+
+age = input ("What is your age :")
+age1 = int(input ("What is your age :"))
+
+print (type(age))
+print ("Current User Entered age is : " + " " + age)
+print ("Current User Entered age is : " + " " + str(age1))
+print (type(age1))
