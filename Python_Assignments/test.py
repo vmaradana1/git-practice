7f20a489e036422c28c9cacaf9865f0ebf8d5076
Vamsi Maradana <vamsitesting@yahoo.com>
2023-06-26 13:59:27 +0100

    updated new code

--- Python_Assignments/test.py
+++ Python_Assignments/test.py
@@ -1,4 +1,25 @@
 #print ('Hello World')
+#username ='bob'
+#age =32
+#print(username,'is',age,'years old')
+#print(username, ' is ' +str(age)+ ' years old')
+
+#username=input('Please enter your name')
+#age = input('Please enter your age')
+#print(username,'is',age,'years old')
+
+length = int(input('please enter length: '))
+width = int(input('please enter width: '))
+
+perimeter = 2* (length + width)
+
+print("Perimeter: ", perimeter)
+
+area = length * width
+
+print("Area: ", area)
+
+
 #x = 10
 #print (type(x))
 #print(f"X value is {x}")
@@ -7,10 +28,10 @@
 #z=True
 #print (type(z))
 
-age = input ("What is your age :")
-age1 = int(input ("What is your age :"))
+#age = input ("What is your age :")
+#age1 = int(input ("What is your age :"))
 
-print (type(age))
-print ("Current User Entered age is : " + " " + age)
-print ("Current User Entered age is : " + " " + str(age1))
-print (type(age1))
+#print (type(age))
+#print ("Current User Entered age is : " + " " + age)
+#print ("Current User Entered age is : " + " " + str(age1))
+#print (type(age1))
