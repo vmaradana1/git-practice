264bb4035856a8aea61a764b27979829f6838719
Vamsi Maradana <vamsitesting@yahoo.com>
2023-06-27 12:31:02 +0100

    updated new code

--- Python_Assignments/Second_Day/labexam2.py
+++ Python_Assignments/Second_Day/labexam2.py
@@ -1,27 +1,26 @@
 
 mark = int(input("Please Enter Your Marks :"))
 level = int(input ("please enter student level :"))
-
 if (level == 1):
     if mark > 70:
         print("Distinction")
-    elif mark >60:
+    elif mark > 60:
         print("Merit")
-    elif mark >50:
+    elif mark > 50:
         print("Pass")
-    elif mark <50:
+    elif mark < 50:
         print("FAIL")
     else:
         print ("Error: marks between 1 and 100")
 else:     
-    if (level == 2 ):
-        if mark >65:
+    if (level == 2):
+        if mark > 65:
             print("Distinction")
-        elif mark >50:
+        elif mark > 50:
             print("Merit")
-        elif mark >40:
+        elif mark > 40:
             print("Pass")
-        elif mark <40:
+        elif mark < 40:
             print("FAIL")
     else:
         print ("Error: marks between 1 and 100")
