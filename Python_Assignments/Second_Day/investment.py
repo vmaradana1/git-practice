48595c458bb0c080cd02e3d98717188399fd3947
Vamsi Maradana <vamsitesting@yahoo.com>
2023-06-27 16:40:35 +0100

    updated new code

--- Python_Assignments/Second_Day/investment.py
+++ Python_Assignments/Second_Day/investment.py
@@ -5,10 +5,15 @@ intialinvestment=100
 IntresetRate = 0.1
 NumberofYears = 1
 
-while NumberofYears > 1:
-    intialinvestment = intialinvestment * IntresetRate
-    NumberofYears +1
-    if (intialinvestment>1000):
+while intialinvestment < 1000 :
+    newcalculation = intialinvestment * IntresetRate
+   # print(newcalculation)
+    intialinvestment=intialinvestment+newcalculation
+   # print(intialinvestment)
+    NumberofYears = NumberofYears + 1
+   # print(NumberofYears)
+    if (intialinvestment>=1000):
         break
-    print(f"Number of {NumberofYears} to reach 1000")
+
+print(f"Number of {NumberofYears} years to reach 1000")
     
