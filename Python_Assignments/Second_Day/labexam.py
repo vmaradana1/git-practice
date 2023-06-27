667b1a7a5e677bda8435af4f8d377389630a694c
Vamsi Maradana <vamsitesting@yahoo.com>
2023-06-27 12:17:41 +0100

    updated new code

---
+++ Python_Assignments/Second_Day/labexam.py
@@ -0,0 +1,12 @@
+
+mark = int(input("Please Enter Your Marks :"))
+if mark >71:
+    print("Distinction")
+elif mark >61:
+    print("Merit")
+elif mark >51:
+    print("Pass")
+elif mark <50:
+    print("FAIL")
+else:
+    print ("invalid marks")
