b7ca62e428ee9158b57892156fb549ea6197d9b0
Vamsi Maradana <vamsitesting@yahoo.com>
2023-06-27 11:59:23 +0100

    updated new code

---
+++ Python_Assignments/Second_Day/labpythogras.py
@@ -0,0 +1,21 @@
+print ("Pythogras Menu :")
+print( "Find Length of A given B & C 1 + \n Find Length of B given A & C 2 \n Find Length of C given A & B 3 ")
+option = input ("Enter option : ")
+if option == 1:
+    B =int(input("Enter value for B : "))
+    C =int(input("Enter value for C : "))
+    output = (C**2)-(B**2)
+    output1 = output ** output
+    print (f"A value is  is {output1}")
+elif option == 2:
+    A =int(input("Enter value for A : "))
+    C =int(input("Enter value for C : "))
+    output = (A**2) -(C**2) 
+    print (f"B value is  is {output}")
+elif option == 3:
+    A =int(input("Enter value for A : "))
+    B =int(input("Enter value for B : "))
+    output = (A**2) -(B**2) 
+    print (f"C value is  is {output}")
+else:
+    print("wrong operation")
