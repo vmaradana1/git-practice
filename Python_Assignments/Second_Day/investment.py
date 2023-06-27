7163d1b4c89807b2cf47441b5042efc1cb7fd8a3
Vamsi Maradana <vamsitesting@yahoo.com>
2023-06-27 16:56:30 +0100

    updated new code

--- Python_Assignments/Second_Day/investment.py
+++ Python_Assignments/Second_Day/investment.py
@@ -2,16 +2,17 @@
 #IntresetRate = input ("Enter IntresetRate : ")
 #TragetValue = input ("Enter TragetValue : ")
 intialinvestment=100
-IntresetRate = 0.1
+IntresetRate = 10
 NumberofYears = 1
 
 while intialinvestment < 1000 :
-    newcalculation = intialinvestment * IntresetRate
-   # print(newcalculation)
-    intialinvestment=intialinvestment+newcalculation
-   # print(intialinvestment)
+    print(f"intialinvestment : {intialinvestment}")
+    newamount = (intialinvestment * 10)/100
+    print(f"newamount : {newamount}")
+    intialinvestment=intialinvestment+newamount
+    print(f"intialinvestment after calculation :{intialinvestment}" )
     NumberofYears = NumberofYears + 1
-   # print(NumberofYears)
+    #print(NumberofYears)
     if (intialinvestment>=1000):
         break
 
