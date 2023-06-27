65e6cc719b476085ab2d4f82561421076b8cc536
Vamsi Maradana <vamsitesting@yahoo.com>
2023-06-27 15:37:10 +0100

    updated new code

--- Python_Assignments/Second_Day/challenge.py
+++ Python_Assignments/Second_Day/challenge.py
@@ -2,27 +2,27 @@ num1=int(input("Enter First Number:"))
 num2=int(input("Enter Second Number:"))
 num3=int(input("Enter Third Number:"))
 
-if (num1 > num2) and (int(num1) > int(num3))):
+if num1 > num2 and num1 >num3:
     print(num1)
-    if ((num1//2) == 0):
+    if ((num1%2) == 0):
         print(f"max number is num1 : {num1} and its even ")
-    elif((num1//3)== 0):
+    elif((num1%3)== 0):
         print(f"max number is num1 : {num1} and its odd and multiple of 3 ")
     else:
         print(f"max number is num1 : {num1} and its odd")       
-elif ((int(num2) > int(num1) and int(num2) > int(num3)):
+elif num2 > num1 and num2 >num3:
     print(num2)
-    if ((num2//2) == 0):
+    if ((num2%2) == 0):
         print(f"max number is num2 : {num2} and its even ")
-    elif((num2//3)== 0):
+    elif((num2%3)== 0):
         print(f"max number is num2 : {num2} and its odd and multiple of 3 ")
     else:
         print(f"max number is num2 : {num2} and its odd")
-else (int(num3) > int(num1) and int(num3) > int(num2)):
+else:
     print(num3)
-    if ((num3//2) == 0):
+    if ((num3%2) == 0):
         print(f"max number is num3 : {num3} and its even ")
-    elif((num3//3)== 0):
+    elif((num3%3)== 0):
         print(f"max number is num3 : {num3} and its odd and multiple of 3 ")
     else:
-        print(f"max number is num3 : {num3} and its odd")
+        print(f"max number is num3 : {num3} and its odd")    
