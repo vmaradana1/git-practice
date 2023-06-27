ac87373833f024a54264b1821b58e350deaac438
Vamsi Maradana <vamsitesting@yahoo.com>
2023-06-27 08:59:21 +0100

    updated new code

--- Python_Assignments/FD_Exercise.py
+++ Python_Assignments/FD_Exercise.py
@@ -1,4 +1,5 @@
 books = {"a1": ["b11","b12"], "a2": ["b21","b22","b23"], "a3": ["b31","b32","b33"]}
 author= input("Enter Author Name: ")
 print (author)
-#print (", ".join(books[author]))
+print (", ".join(books[author]))
+print (", ".join(books.get(author)))
