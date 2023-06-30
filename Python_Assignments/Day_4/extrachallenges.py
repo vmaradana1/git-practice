15b3d3141596db7fc81648f6e2d92af485dd2ebd
Vamsi Maradana <vamsitesting@yahoo.com>
2023-06-30 15:38:29 +0100

    updated new code

--- Python_Assignments/Day_4/extrachallenges.py
+++ Python_Assignments/Day_4/extrachallenges.py
@@ -1,6 +1,7 @@
 #1. Create a Rectangle class with attributes length and width. 
 #Add methods to calculate the area and perimeter of the rectangle. 
 
+"""
 class Rectangle:
     def __init__(self, length, width):
         self.length = length
@@ -20,13 +21,104 @@ print(rect.perimiter())
 #2. Create a BankAccount class with attributes account_number and balance. 
 #Add methods to deposit and withdraw money from the account. 
 
+class BankAccount:
+    def __init__(self,account_number,depositbalance=0):
+        self.account_number = account_number
+        self.depositbalance = depositbalance
+    
+    def deposit(self,depositamount):
+        self.depositbalance += depositamount
+        return depositamount
+    
+    def withdraw(self,withdrawlamount):
+        self.depositbalance -= withdrawlamount
+        return withdrawlamount
+        
+    def displaybalance(self):
+        return self.depositbalance
+     
+bankdetails=BankAccount(23)
+balancebeforedeposit=bankdetails.deposit(1000)
+balanceafteredeposit=bankdetails.displaybalance()
+balancebeforewithdrawl=bankdetails.withdraw(500)
+balanceafterwithdrawl=bankdetails.displaybalance()
+
+print(f"New Account balance after depositing {balancebeforedeposit} is : {balanceafteredeposit}")
+print(f"New Account balance after withdrawing {balancebeforewithdrawl} is : {balanceafterwithdrawl}")
+
 
 #3. Create a Car class with attributes make, model, and year. 
 #Add methods to get and set the values of the attributes. 
 
+class Car:
+    def __init__(self,model,make,year):
+
+        self.model = model
+        self.make = make
+        self.year = year
+            
+    def get_make(self):  
+        return self.make
+    
+    def get_model(self):  
+        return self.model
+    
+    def get_year(self):  
+        return self.year
+    
+    def set_make(self,updatedmake):
+        self.make=updatedmake
+    
+    def set_model(self,updatedmodel):
+        self.model=updatedmodel              
+    
+    def set_year(self,updatedyear):
+        self.year=updatedyear    
+        
+car1=Car('model1','make1',2009)
+print(car1.get_make())
+car1.set_make("make11")
+print(car1.get_make())
+   
+
+    
+
 #4. Create a Product class with attributes name, price, and quantity. 
 #Add methods to calculate the total value of the product (price * quantity) 
 #and add or remove items from the product inventory. 
+
+class Product:
+    def __init__(self,name,price=0,quantity=0):
+        self.name=name
+        self.price=price
+        self.quantity=quantity
+    
+    def tvalue(self):
+        totalvalue = self.price * self.quantity
+        return totalvalue
+    
+    def addproducts(self,addquantity):
+        addquantity = self.quantity+addquantity
+        return addquantity
+    
+    def removeproducts(self,removequantity):
+        removequantity = self.quantity - removequantity
+        return removequantity
+    
+
+items = Product("Test",10,100)
+#print(f"Total value is : {items}")
+
+Totalvalue=items.tvalue()  
+print(f"Total value is : {Totalvalue}")
+
+AddQuantity=items.addproducts(10)
+print(f"Total updated Quantity is : {AddQuantity}")
+
+removedQuantity=items.removeproducts(10)
+print(f"Total updated Quantity is : {removedQuantity}")
+
+"""
 
 #Harder challenge (stretch goal): 
 
@@ -46,6 +138,8 @@ print(rect.perimiter())
 #and returns a list of book objects if found 
 #The class should also have a str method that returns a string representation 
 #of the books on the shelf. 
+
+
 
 #Create four Book objects and add them to a BookShelf object with a capacity of three. 
 #Test the BookShelf object by adding, removing, and finding books by title and author.
