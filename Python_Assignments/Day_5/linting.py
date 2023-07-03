a39c3ddabe673667525c1585d79123ff58757bba
Vamsi Maradana <vamsitesting@yahoo.com>
2023-07-03 16:45:27 +0100

    test

---
+++ Python_Assignments/Day_5/linting.py
@@ -0,0 +1,12 @@
+'''
+Example Doc string
+'''
+
+def num_count(sequence, item):
+    '''Function for count'''
+    variable_1 = 0
+    for variable_2 in sequence:
+        if variable_2 == item:
+            variable_1 += 1
+    return variable_1
+  
