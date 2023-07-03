53e2083c60511c1b1f3f6667c85234523adcd852
Vamsi Maradana <vamsitesting@yahoo.com>
2023-07-03 16:14:53 +0100

    new code

--- Python_Assignments/Day_5/testing.py
+++ Python_Assignments/Day_5/testing.py
@@ -35,10 +35,31 @@ def fact(x):
         return 1
     return x * fact(x - 1)
 
-def test_count_int():
-    assert count(5) == 120
-    assert count(0) == 1
-    assert count(1) == 1
-    assert count(-3) == 6
+def test_fact():
+    assert fact(5) == 120
+    assert fact(0) == 1
+    assert fact(1) == 1
 
+
+def vowels(word):
+    number_of_vowels = 0
+    the_vowels = ["a", "e", "i", "o", "u"]
+    for letter in word.lower():
+        if letter in the_vowels:
+            number_of_vowels += 1
+    return number_of_vowels
+
+def test_vowel_count():
+    assert vowels("orange") == 3
+    assert vowels("PULP") == 1
+    assert vowels("ORANGE") == 3
     
+    
+def list_of_squares(n):
+    d=dict()
+    for i in range(1,n+1):
+        d[i]=i*i
+    return d
+
+def test_list_of_squares():
+    assert list_of_squares(2) == {1:1,2:4}    
