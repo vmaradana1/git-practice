7699f54a2737c25ce1e8e2895df45afbfa57ce21
Vamsi Maradana <vamsitesting@yahoo.com>
2023-07-04 15:33:25 +0100

    new code

--- Python_Assignments/Day_5/exercise.py
+++ Python_Assignments/Day_5/exercise.py
@@ -1,20 +1,25 @@
-# Python program to read CSV file line by line
-# import necessary packages
 import csv
+
 companies = []
 sales = []
 
-with open('carsale.csv') as csvfile: # dont need r/w attribiute for csv 
-    reader = csv.reader(csvfile)
-    next(reader) # skipping header 
-    for row in reader: #row is variable:
+with open("output.csv") as csvfile: # dont need r or w as csv module handles this. 
+    reader = csv.reader(csvfile) # reader function from the cvs module, which reads all the lines.
+    next(reader) # skips the first line dont need the header data
+    for row in reader:
         companies.append(row[0])
-        sales.append([int(x.replace(",",""))for x in row[1:]])
-        
-print(sales[1])
-total = 0
-for num in sales[1]:
-    total += num
+        sales.append([int(x.replace(",", "")) for x in row[1:]]) # iterates from index 1 to the end, changes from str to int and gets rid of ","
 
-print(total)
+print(sales)
+monthly_sum = [sum(x) for x in zip(*sales)] # unpacks the list of lists into tuples, and sums.
 
+print(monthly_sum)
+
+yearly_sum = {}
+for i in range(len(companies)):
+    yearly_sum[companies[i]] = sum(sales[i])
+
+print("monthly sales:", monthly_sum)
+print("yearly sales:")
+for company, sales in yearly_sum.items():
+    print(company, sales)
